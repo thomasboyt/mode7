4a54c7368c38eba809ebe504b80bac172be4854b
Thomas Boyt <me@thomasboyt.com>
2014-11-08 20:14:46 -0500

    Convert to Coquette game

---
+++ src/World.js
@@ -0,0 +1,63 @@
+var mode7 = require('./renderer');
+
+function imageDataFromImg(img) {
+  // draw original to an in-memory canvas
+  var canvas = document.createElement('canvas');
+  canvas.width = img.width;
+  canvas.height = img.height;
+
+  var ctx = canvas.getContext('2d');
+  ctx.drawImage(img, 0, 0);
+  return ctx.getImageData(0, 0, img.width, img.height);
+}
+
+class World {
+  constructor(game, opts) {
+    this.game = game;
+
+    this.textureImageData = imageDataFromImg(opts.texture);
+    this.position = opts.position;
+    this.horizOffset = opts.horizOffset;
+    this.config = opts.mode7Config;
+  }
+
+  draw(ctx) {
+    var target = ctx.createImageData(this.game.width, this.game.height);
+
+    var rad = this.position.angle * (Math.PI/180);
+    mode7(target, this.textureImageData, this.position.x, this.position.y, rad, this.config);
+
+    ctx.putImageData(target, 0, this.horizOffset, 0, 0,
+                     this.game.width, this.game.height - this.horizOffset);
+  }
+
+  _move(step) {
+    var angle = this.position.angle * Math.PI/180;
+    var dx = Math.cos(angle) * step;
+    var dy = Math.sin(angle) * step;
+
+    this.position.x = this.position.x + dx;
+    this.position.y = this.position.y + dy;
+  }
+
+  update(dt) {
+    var c = this.game.c;
+
+    var step = 10 * dt/100;
+    var turnStep = 10 * dt/100;
+
+    if (c.inputter.isDown(c.inputter.W)) {
+      this._move(step);
+    } else if (c.inputter.isDown(c.inputter.S)) {
+      this._move(-step);
+    }
+
+    if (c.inputter.isDown(c.inputter.A)) {
+      this.position.angle -= turnStep;
+    } else if (c.inputter.isDown(c.inputter.D)) {
+      this.position.angle += turnStep;
+    }
+  }
+}
+
+module.exports = World;
--- src/main.js
+++ src/main.js
@@ -1,104 +1,39 @@
-var mode7 = require('./renderer');
+var Coquette = require('coquette');
 var mapUrl = require('../assets/crop_map.png');
+var World = require('./World');
 
-function imageDataFromImage(img) {
-  // draw original to an in-memory canvas
-  var canvas = document.createElement('canvas');
-  canvas.width = img.width;
-  canvas.height = img.height;
+class Game {
+  constructor(image) {
+    this.width = 500;
+    this.height = 250;
 
-  var ctx = canvas.getContext('2d');
-  ctx.drawImage(img, 0, 0);
-  return ctx.getImageData(0, 0, img.width, img.height);
+    this.c = window.__coquette__ = new Coquette(this, 'canvas', this.width, this.height, 'black');
+
+    var world = this.c.entities.create(World, {
+      texture: image,
+
+      position: {
+        x: 877,
+        y: 605,
+        angle: 270
+      },
+      mode7Config: {
+        spaceZ: 15,
+        scaleX: 300,
+        scaleY: 300,
+        horizon: 6  // todo: wtf?
+      },
+      horizOffset: 50
+    });
+  }
 }
 
-var Mode7Manager = function(canvas, img) {
-  this.canvas = canvas;
-  this.originMap = imageDataFromImage(img);
-
-  this.position = {
-    x: 877,
-    y: 605,
-    angle: 270
-  };
-
-  this.config = {
-    spaceZ: 15,
-    scaleX: 300,
-    scaleY: 300,
-    horizon: 6  // todo: wtf?
-  };
-
-  this.horizOffset = 50;
-
-  var runLoop = () => {
-    this.render();
-    window.requestAnimationFrame(runLoop);
-  };
-
-  window.requestAnimationFrame(runLoop);
-};
-
-Mode7Manager.prototype.render = function() {
-  var ctx = this.canvas.getContext('2d');
-  var target = ctx.createImageData(canvas.width, canvas.height);
-
-  var rad = this.position.angle * (Math.PI/180);
-  mode7(target, this.originMap, this.position.x, this.position.y, rad, this.config);
-
-  ctx.putImageData(target, 0, this.horizOffset, 0, 0, this.canvas.width, this.canvas.height - this.horizOffset);
-};
-
-// Move forward or backward by step based on current angle
-Mode7Manager.prototype._move = function(step) {
-
-  var angle = this.position.angle * Math.PI/180;
-  var dx = Math.cos(angle) * step;
-  var dy = Math.sin(angle) * step;
-
-  this.position.x = this.position.x + dx;
-  this.position.y = this.position.y + dy;
-};
-
-Mode7Manager.prototype.handleKeyDown = function(e) {
-  var keyCode = e.keyCode;
-
-  var up = 87;      // W
-  var down = 83;    // S
-  var left = 65;    // A
-  var right = 68;   // D
-
-  var step = 3;
-  var turnStep = 1;
-
-  if (keyCode === up) {
-    this._move(step);
-  } else if (keyCode === down) {
-    this._move(-step);
-
-  } else if (keyCode === left) {
-    this.position.angle -= turnStep;
-  } else if (keyCode === right) {
-    this.position.angle += turnStep;
-  }
-
-  // this.render();
-};
-
+// TODO: replace this with something like AssetPreloader
 function init() {
-  var canvas = document.getElementById('canvas');
-  canvas.width = 500;
-  canvas.height = 250;
-
   var img = new Image();
 
   img.onload = function() {
-
-    var mode7 = new Mode7Manager(canvas, img);
-    window.onkeydown = (e) => { mode7.handleKeyDown(e); };
-
-    global.mode7 = mode7;
-
+    new Game(img);
   };
 
   img.src = mapUrl;
